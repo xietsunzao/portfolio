3343cf0c22d991360b14d8456973051258f68ec8
xietsunzao <lordshinjo22@gmail.com>
2025-01-02 14:42:13 +0700

    Add redirects for jefrimaruli.dev to enforce HTTPS and improve site accessibility

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'jefrimaruli.dev',
+          },
+        ],
+        destination: 'https://www.jefrimaruli.dev/:path*',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
